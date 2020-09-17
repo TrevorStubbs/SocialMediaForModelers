52c23e2d020e03ae4373289356879d35213df448
Trevor Stubbs <stubbste@gmail.com>
2020-09-16 18:47:57 -0700

    Initial Model Setup

---
+++ SocialMediaForModelers/Model/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string UserId { get; set; }
+        public string Caption{ get; set; }        
+        public List<PostLike> PostLikes { get; set; }
+        public List<PostComment> PostComments { get; set; }
+        public List<PostImage> PostImages { get; set; }
+
+    }
+}
---
+++ SocialMediaForModelers/Model/PostImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model
+{
+    public class PostImage
+    {
+        public int ID { get; set; }
+        public int PostId { get; set; }
+        public string ImageURI { get; set; } // Will need to change once S3 is setup
+    }
+}
