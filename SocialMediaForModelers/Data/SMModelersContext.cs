0285b6100cd8f3cc0a8228706b7e8cf5b5fe7512
Trevor Stubbs <stubbste@gmail.com>
2020-09-26 12:12:46 -0700

    Initial DbContext Setup

--- SocialMediaForModelers/Data/SMModelersContext.cs
+++ SocialMediaForModelers/Data/SMModelersContext.cs
@@ -1,8 +1,11 @@
-﻿using SocialMediaForModelers.Model;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaForModelers.Model;
+using SocialMediaForModelers.Model.Entities.JoinEntites;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
 namespace SocialMediaForModelers.Data
@@ -18,14 +21,37 @@ namespace SocialMediaForModelers.Data
         {
             base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<AppUserFriend>().HasKey(x => new { x.UserId, x.FriendId });
+
+            modelBuilder.Entity<PostToComment>().HasKey(x => new { x.UserPageId, x.CommentId });
+            modelBuilder.Entity<PostToImage>().HasKey(x => new { x.PostId, x.PhotoId });
+            modelBuilder.Entity<UserPageToPost>().HasKey(x => new { x.UserPageId, x.PostId });
+            modelBuilder.Entity<CommentLike>().HasKey(x => new { x.CommentId, x.UserId });
+            modelBuilder.Entity<PageLike>().HasKey(x => new { x.PageId, x.UserId });
+            modelBuilder.Entity<PostLike>().HasKey(x => new { x.PostId, x.UserId });
+
             modelBuilder.Entity<ApplicationUser>().HasData(
                 new ApplicationUser
                 {
                     UserId = "1234",
                     FirstName = "Trevor",
                     LastName = "Stubbs",
+                    Email = "stubbste@gmail.com",
+                    UserName = "stubbste@gmail.com",
                     DOB = new DateTime(1982, 6, 8)
                 });
         }
+
+        // Normal Tables
+        public DbSet<ApplicationUser> AppUsers { get; set; }
+        public DbSet<PostComment> PostComments { get; set; }
+        public DbSet<PostImage> PostImages { get; set; }
+        public DbSet<UserPage> UserPages { get; set; }
+        public DbSet<UserPost> UserPosts { get; set; }
+        // Join Tables
+        public DbSet<AppUserFriend> UserFriends { get; set; }
+        public DbSet<PostToComment> PostToComments { get; set; }
+        public DbSet<PostToImage> PostToImages { get; set; }
+        public DbSet<UserPageToPost> UserPageToPosts { get; set; }
     }
 }
