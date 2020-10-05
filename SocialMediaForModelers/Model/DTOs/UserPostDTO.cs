1ab96458fbb62ce0e91ece8e12605527f5b1c34a
Trevor Stubbs <stubbste@gmail.com>
2020-10-05 12:16:42 -0700

    Interface Scaffold

---
+++ SocialMediaForModelers/Model/DTOs/UserPostDTO.cs
@@ -0,0 +1,20 @@
+﻿using SocialMediaForModelers.Model.Entities.JoinEntites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model.DTOs
+{
+    public class UserPostDTO
+    {
+        public int Id { get; set; }
+        public string Caption { get; set; }
+
+        // Navigation properties
+        public List<UserPageToPost> UserPageToPosts { get; set; }
+        public List<PostLike> PostLikes { get; set; }
+        public List<PostToImage> PostToImages { get; set; }
+        public List<PostToComment> PostToComments { get; set; }
+    }
+}
---
+++ SocialMediaForModelers/Model/Interfaces/IUserPost.cs
@@ -0,0 +1,54 @@
+﻿using SocialMediaForModelers.Model.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model.Interfaces
+{
+    public interface IUserPost
+    {
+        // PostCRUD
+        //  Create a Post
+        Task<UserPostDTO> Create(UserPostDTO post, string userId);
+
+        //  GetAllPosts
+        Task<List<UserPostDTO>> GetAllPosts();
+
+        //  GetAllOfAUsersPosts
+        Task<List<UserPostDTO>> GetAllUserPosts(string userId);
+
+        //  GetASpecificPost
+        Task<UserPostDTO> GetASpecificPost(int postId);
+
+        //  Update
+        Task<UserPostDTO> Update(UserPostDTO post);
+
+        //  Delete a Post
+        Task Delete(int postId);
+
+        // LikeCRUD
+        //  Add a like to a post
+        Task AddALikeToAPost(int postId, string userId);
+
+        //  Get the number of likes for a post
+        Task<LikeDTO> GetPostLikes(int postId, string userId);
+
+        //  Delete a like from a post
+        Task DeleteALike(int postId, string userId);
+
+        // Image Join Table CRUD
+        //  Add an image to a post
+        Task AddAnImageToAPost(int postId, int imageId);
+
+        //  Delete an image from a post
+        Task DeleteAnImageFromAPost(int postId, int imageId);
+
+        // Comment Join Table CRUD
+        //  Add a comment to a post
+        Task AddACommentToAPost(int postId, int commentId);
+
+        //  Delete a comment from a post
+        Task DeleteACommentFromAPost(int postId, int commentId);
+    }
+}
