09b46308a46da5b4b4203f66f2e8c2149cb22dd6
Trevor Stubbs <stubbste@gmail.com>
2020-10-18 09:54:20 -0700

    AppRolls and Role Initilizer

---
+++ SocialMediaForModelers/Model/AppRoles/RoleInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model.AppRoles
+{
+    public class RoleInitializer
+    {
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+{
+            new IdentityRole
+            {
+                Name = ApplicationRoles.Admin, 
+                NormalizedName = ApplicationRoles.Admin.ToUpper(), 
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            },
+            new IdentityRole
+            {
+                Name = ApplicationRoles.User, 
+                NormalizedName = ApplicationRoles.User.ToUpper(), 
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            }
+        };
+
+
+    }
+}
