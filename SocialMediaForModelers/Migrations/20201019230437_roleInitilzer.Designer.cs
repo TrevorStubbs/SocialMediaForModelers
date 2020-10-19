212daf91495316049fb13101e7377b0947342420
Trevor Stubbs <stubbste@gmail.com>
2020-10-19 16:10:07 -0700

    Role Initializer finished, migration and update with identity.

--- SocialMediaForModelers/Migrations/20201019230437_roleInitilzer.Designer.cs
+++ SocialMediaForModelers/Migrations/20201019230437_roleInitilzer.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialMediaForModelers.Data;
 
 namespace SocialMediaForModelers.Migrations
 {
     [DbContext(typeof(SMModelersContext))]
-    partial class SMModelersContextModelSnapshot : ModelSnapshot
+    [Migration("20201019230437_roleInitilzer")]
+    partial class roleInitilzer
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
