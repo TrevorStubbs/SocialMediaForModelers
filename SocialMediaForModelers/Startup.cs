f0ce3a6907f23223a584dc381fbeb3614dd9839e
Trevor Stubbs <stubbste@gmail.com>
2020-10-19 16:59:12 -0700

    JWToken Initial setup

--- SocialMediaForModelers/Startup.cs
+++ SocialMediaForModelers/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,8 +15,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using SocialMediaForModelers.Data;
 using SocialMediaForModelers.Model;
+using SocialMediaForModelers.Model.AppRoles;
 using SocialMediaForModelers.Model.Interfaces;
 using SocialMediaForModelers.Model.Managers;
 
@@ -50,6 +54,33 @@ namespace SocialMediaForModelers
                     .AddEntityFrameworkStores<SMModelersContext>()
                     .AddDefaultTokenProviders();
 
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = false,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["JWTIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWTKey"]))
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("AdminPriv", policy => policy.RequireRole(ApplicationRoles.Admin));
+                options.AddPolicy("UserPriv", policy => policy.RequireRole(ApplicationRoles.Admin, ApplicationRoles.User));
+            });
+
+
+
             services.AddTransient<IPostComment, PostCommentManager>();
             services.AddTransient<IPostImage, PostImageManager>();
             services.AddTransient<IUserPost, UserPostManager>();
@@ -57,7 +88,7 @@ namespace SocialMediaForModelers
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -66,13 +97,18 @@ namespace SocialMediaForModelers
 
             app.UseHttpsRedirection();
 
+            app.UseStaticFiles();
+
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute("default", "{controller=home}/{id?}");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
