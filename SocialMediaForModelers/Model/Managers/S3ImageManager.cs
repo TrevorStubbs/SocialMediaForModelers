2efdcc5162d4835009fabdcc12223edf19b55001
Trevor Stubbs <stubbste@gmail.com>
2020-10-28 23:04:46 -0700

    s3 image manager

--- SocialMediaForModelers/Model/Managers/S3ImageManager.cs
+++ SocialMediaForModelers/Model/Managers/S3ImageManager.cs
@@ -1,4 +1,5 @@
-﻿using SocialMediaForModelers.Model.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using SocialMediaForModelers.Model.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,5 +9,31 @@ namespace SocialMediaForModelers.Model.Managers
 {
     public class S3ImageManager : IS3Image
     {
+        private IConfiguration _config;
+
+        public S3ImageManager(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Task<string> AddABucket(string bucketName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteABucket(string bucketName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> AddAnImageToABucket()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAnImageFromABucket(string imageId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
