4fbc6bdb1ddedf8cf2fc5fc4ddcb897bf8597319
Trevor Stubbs <stubbste@gmail.com>
2020-11-05 11:30:39 -0800

    s3 image manager

---
+++ SocialMediaForModelers/Model/Interfaces/ICloudImage.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model.Interfaces
+{
+    public interface ICloudImage
+    {
+        // Add an image
+        Task<HttpStatusCode> AddAnImageToABucket(string imageId, IFormFile imageFile);
+        // Get image url
+        string GetImageUrl(string imageId);
+        // Delete an image
+        Task<HttpStatusCode> DeleteAnImageFromABucket(string imageId);
+    }
+}
