738df7dfc279dee94a98dbe9acca27bbbdedd4c7
Trevor Stubbs <stubbste@gmail.com>
2020-11-06 19:13:32 -0800

    JWT Token setup and Account controller built

---
+++ SocialMediaForModelers/Model/DTOs/AssignRoleDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model.DTOs
+{
+    public class AssignRoleDTO
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ SocialMediaForModelers/Model/DTOs/LoginDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Model.DTOs
+{
+    public class LoginDTO
+    {
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
