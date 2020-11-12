8b866e1bf1ab8e71241fb802c9397e87305d6863
Trevor Stubbs <stubbste@gmail.com>
2020-11-11 18:27:03 -0800

    Half methods are finished

--- SocialMediaForModelers/Controllers/UserPostController.cs
+++ SocialMediaForModelers/Controllers/UserPostController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SocialMediaForModelers.Controllers.ControllerSupport;
+using SocialMediaForModelers.Model.DTOs;
 using SocialMediaForModelers.Model.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -23,18 +25,87 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // POST: /UserPost
+        public async Task<ActionResult<UserPostDTO>> PostUserPost(UserPostDTO newPost)
+        {
+            var post = await _userPost.Create(newPost);
+
+            if (post != null)
+            {
+                return post;
+            }
+
+            return BadRequest();
+        }
 
         // GET: /UserPost
+        public async Task<ActionResult<List<UserPostDTO>>> GetAllPosts()
+        {
+            var posts = await _userPost.GetAllPosts();
+
+            if (posts != null)
+            {
+                return posts;
+            }
+
+            return BadRequest();
+        }
 
         // GET: /UserPost/UserId
+        public async Task<ActionResult<List<UserPostDTO>>> GetUsersUserPosts()
+        {
+            var posts = await _userPost.GetAllUserPosts(UserClaimsGetters.GetUserId(User));
+
+            if (posts != null)
+            {
+                return posts;
+            }
+
+            return BadRequest();
+        }
 
         // GET: /UserPost/{postId}
+        public async Task<ActionResult<UserPostDTO>> GetAPost(int postId)
+        {
+            var post = await _userPost.GetASpecificPost(postId);
+
+            if (post != null)
+            {
+                return post;
+            }
+
+            return BadRequest();
+        }
 
         // PUT: /UserPost/{postId}
+        public async Task<ActionResult<UserPostDTO>> PutUserPost(UserPostDTO updatePost, int postId)
+        {
+            var post = await _userPost.Update(updatePost, postId);
+
+            if (post != null)
+            {
+                return post;
+            }
+
+            return BadRequest();
+        }
 
         // DELETE: /UserPost/{postId}
+        public async Task<IActionResult> DeletePost(int postId)
+        {
+            try
+            {
+                await _userPost.Delete(postId);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Delete action exception message: {e.Message}");
+            }
+        }
 
         // POST: /UserPost/{postId}/Like
+
 
         // GET: /UserPost/{postId}/Like
 
@@ -45,7 +116,7 @@ namespace SocialMediaForModelers.Controllers
         // DELETE: /UserPost/{postId}/{imageId}
 
         // POST: /UserPost/{postId}/{commentId}
-        
+
         // DELETE: /UserPost/{postId}/{commentId}
 
     }
