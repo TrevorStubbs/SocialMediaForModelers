849d8698a537ea96ef9670a8ea9e721dfd022a73
Trevor Stubbs <stubbste@gmail.com>
2020-11-13 16:08:53 -0800

    UserPostController Methods complete

--- SocialMediaForModelers/Controllers/UserPostController.cs
+++ SocialMediaForModelers/Controllers/UserPostController.cs
@@ -13,7 +13,7 @@ namespace SocialMediaForModelers.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [AllowAnonymous]
+    [AllowAnonymous] // Temporary - Change after testing
 
     public class UserPostController : ControllerBase
     {
@@ -25,6 +25,7 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // POST: /UserPost
+        [HttpPost]
         public async Task<ActionResult<UserPostDTO>> PostUserPost(UserPostDTO newPost)
         {
             var post = await _userPost.Create(newPost);
@@ -38,6 +39,7 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // GET: /UserPost
+        [HttpGet]
         public async Task<ActionResult<List<UserPostDTO>>> GetAllPosts()
         {
             var posts = await _userPost.GetAllPosts();
@@ -51,6 +53,7 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // GET: /UserPost/UserId
+        [HttpGet("UserId")]
         public async Task<ActionResult<List<UserPostDTO>>> GetUsersUserPosts()
         {
             var posts = await _userPost.GetAllUserPosts(UserClaimsGetters.GetUserId(User));
@@ -64,6 +67,7 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // GET: /UserPost/{postId}
+        [HttpGet("{postId}")]
         public async Task<ActionResult<UserPostDTO>> GetAPost(int postId)
         {
             var post = await _userPost.GetASpecificPost(postId);
@@ -77,8 +81,14 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // PUT: /UserPost/{postId}
+        [HttpPut("{postId}")]
         public async Task<ActionResult<UserPostDTO>> PutUserPost(UserPostDTO updatePost, int postId)
         {
+            if (postId != updatePost.Id)
+            {
+                return BadRequest();
+            }
+
             var post = await _userPost.Update(updatePost, postId);
 
             if (post != null)
@@ -90,8 +100,13 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // DELETE: /UserPost/{postId}
+        [HttpDelete("{postId}")]
         public async Task<IActionResult> DeletePost(int postId)
         {
+            // Test to see if claim == post.UserId or policy is admin
+            // if so allow the delete
+            // if not don't allow it
+
             try
             {
                 await _userPost.Delete(postId);
@@ -105,19 +120,119 @@ namespace SocialMediaForModelers.Controllers
         }
 
         // POST: /UserPost/{postId}/Like
+        [HttpPost("{postId}/Like")]
+        public async Task<IActionResult> PostPostLike(int postId)
+        {
+            try
+            {
+                await _userPost.AddALikeToAPost(postId, UserClaimsGetters.GetUserId(User));
 
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Tried to add a like to a post: {e.Message}");
+            }
+        }
 
         // GET: /UserPost/{postId}/Like
+        [HttpGet("{postId}/Like")]
+        public async Task<ActionResult<LikeDTO>> GetPostLikes(int postId)
+        {
+            var likeData = await _userPost.GetPostLikes(postId, UserClaimsGetters.GetUserId(User));
+
+            if (likeData != null)
+            {
+                return likeData;
+            }
+
+            return BadRequest();
+        }
 
         // DELETE: /UserPost/{postId}/Like
+        [HttpDelete("{postId}/Like")]
+        public async Task<IActionResult> DeletePostLike(int postId)
+        {
+            try
+            {
+                await _userPost.DeleteALike(postId, UserClaimsGetters.GetUserId(User));
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Cannot delete that like: {e.Message}");
+            }
+        }
 
         // POST: /UserPost/{postId}/{imageId}
+        [HttpPost("{postId}/Image/{imageId}")]
+        public async Task<IActionResult> PostAnImageToPost(int postId, int imageId)
+        {
+            // ====================== TODO: How are we going to deal with S3? =========================
+            // Do we have this controller call the PostImage Controller?
+            // Do we make the client do it?
+            // ========================================================================================
+            try
+            {
+                await _userPost.AddAnImageToAPost(postId, imageId);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Cannot add the image to the post: {e.Message}");
+            }
+        }
 
         // DELETE: /UserPost/{postId}/{imageId}
+        [HttpDelete("{postId}/Image/{imageId}")]
+        public async Task<IActionResult> DeleteAnImageFromPost(int postId, int imageid)
+        {
+            // ====================== TODO: Same here how are we going to deal with S3 ===============            
+            try
+            {
+                await _userPost.DeleteAnImageFromAPost(postId, imageid);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Cannot delete the image from the post: {e.Message}");
+            }
+        }
 
         // POST: /UserPost/{postId}/{commentId}
+        [HttpPost("{postId}/Comment/{imageId}")]
+        public async Task<IActionResult> PostACommentToPost(int postid, int commentId)
+        {
+            try
+            {
+                await _userPost.AddACommentToAPost(postid, commentId);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {                
+                throw new Exception($"Cannot add the comment to the post: {e.Message}");
+            }
+        }
 
         // DELETE: /UserPost/{postId}/{commentId}
+        [HttpDelete("{postId}/Comment/{imageId}")]
+        public async Task<IActionResult> DeleteACommentFromPost(int postId, int commentId)
+        {
+            try
+            {
+                await _userPost.DeleteACommentFromAPost(postId, commentId);
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"Cannot delete the comment from the post: {e.Message}");
+            }
+        }
 
     }
 }
