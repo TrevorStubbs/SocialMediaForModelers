eb68cc1c50cb47cd7f6da477a311cb1a4d52ac8d
Trevor Stubbs <stubbste@gmail.com>
2020-11-14 14:10:20 -0800

    PostCommentController scaffold

---
+++ SocialMediaForModelers/Controllers/PostCommentsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaForModelers.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMediaForModelers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // ===================== TODO: Change to Restricted =============================
+    [AllowAnonymous]
+    public class PostCommentsController : ControllerBase
+    {
+        private readonly IPostComment _postComment;
+
+        public PostCommentsController(IPostComment postComment)
+        {
+            _postComment = postComment;
+        }
+
+        // POST: /PostComment
+
+        // GET: /PostComment
+
+        // GET: /PostComment/UserId
+
+        // GET: /PostComment/{postId}/post
+
+        // GET: /PostComment/{commentId}
+
+        // PUT: /PostComment/{commentId}
+
+        // DELETE: /PostComment/{commentId}
+
+    }
+}
